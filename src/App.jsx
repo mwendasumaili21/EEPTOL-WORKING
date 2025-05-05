e03cb91bac7a87d8b5abcd9eb670b9454a24a866
mwanda <mwendasumaili21@gmail.com>
2025-05-05 21:25:01 +0200

    Feature: Add navigation bar for Home, Topics, and Help

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <div>
+      <NavBar />
       <AppRoutes />
     </div>
   );
 }
-
